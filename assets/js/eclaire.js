b25f4c13c13d422a27525e86aab7b2ce65e38683
Maxime <maximeringeval@orange.fr>
2022-02-25 15:21:29 +0100

    lum_js

--- assets/js/eclaire.js
+++ assets/js/eclaire.js
@@ -61,3 +61,97 @@ img_trois.src = eclairage.produits[2].imgurl;
 name_trois.textContent = eclairage.produits[2].nom;
 ref_trois.textContent = eclairage.produits[2].référence;
 price_trois.textContent = eclairage.produits[2].prix;
+
+
+
+
+
+
+let row_deux = document.createElement("div");
+row_deux.classList.add("div_deux");
+
+let card_quatre = document.createElement("div");
+let card_cinq = document.createElement("div");
+let card_six = document.createElement("div");
+card_quatre.classList.add("card");
+card_cinq.classList.add("card");
+card_six.classList.add("card");
+
+let img_quatre = document.createElement("img");
+let img_cinq = document.createElement("img");
+let img_six = document.createElement("img");
+
+let name_quatre = document.createElement("p");
+let name_cinq = document.createElement("p");
+let name_six = document.createElement("p");
+
+let ref_quatre = document.createElement("p");
+let ref_cinq = document.createElement("p");
+let ref_six = document.createElement("p");
+
+let price_quatre = document.createElement("p");
+let price_cinq = document.createElement("p");
+let price_six = document.createElement("p");
+
+
+content.appendChild(row_deux);
+row_deux.appendChild(card_quatre);
+row_deux.appendChild(card_cinq);
+row_deux.appendChild(card_six);
+
+card_quatre.appendChild(img_quatre);
+card_cinq.appendChild(img_cinq);
+card_six.appendChild(img_six);
+
+card_quatre.appendChild(name_quatre);
+card_cinq.appendChild(name_cinq);
+card_six.appendChild(name_six);
+
+card_quatre.appendChild(ref_quatre);
+card_cinq.appendChild(ref_cinq);
+card_six.appendChild(ref_six);
+
+card_quatre.appendChild(price_quatre);
+card_cinq.appendChild(price_cinq);
+card_six.appendChild(price_six);
+
+img_quatre.src = eclairage.produits[3].imgurl;
+name_quatre.textContent = eclairage.produits[3].nom;
+ref_quatre.textContent = eclairage.produits[3].référence;
+price_quatre.textContent = eclairage.produits[3].prix;
+
+img_cinq.src = eclairage.produits[4].imgurl;
+name_cinq.textContent = eclairage.produits[4].nom;
+ref_cinq.textContent = eclairage.produits[4].référence;
+price_cinq.textContent = eclairage.produits[4].prix;
+
+img_six.src = eclairage.produits[5].imgurl;
+name_six.textContent = eclairage.produits[5].nom;
+ref_six.textContent = eclairage.produits[5].référence;
+price_six.textContent = eclairage.produits[5].prix;
+
+
+
+
+
+
+
+let row_trois = document.createElement("div");
+row_trois.classList.add("div_trois");
+let card_sept = document.createElement("div");
+card_sept.classList.add("card");
+let img_sept = document.createElement("img");
+let name_sept = document.createElement("p");
+let ref_sept = document.createElement("p");
+let price_sept = document.createElement("p");
+content.appendChild(row_trois);
+row_trois.appendChild(card_sept);
+card_sept.appendChild(img_sept);
+card_sept.appendChild(name_sept);
+card_sept.appendChild(ref_sept);
+card_sept.appendChild(price_sept);
+
+img_sept.src = eclairage.produits[6].imgurl;
+name_sept.textContent = eclairage.produits[6].nom;
+ref_sept.textContent = eclairage.produits[6].référence;
+price_sept.textContent = eclairage.produits[6].prix;
