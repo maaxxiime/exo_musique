6947476137eb61e5b0ac891afe51ad4b6c983e7a
marctiziou <97169879+marctiziou@users.noreply.github.com>
2022-03-01 15:45:31 +0100

    01/03/2022

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+function connect(){
+  let user="iorana"
+  let mdp="ght1velo"
+  let userinput = document.getElementById("butt").value;
+  let mdpinput = document.getElementById("butt1").value;
+  if ( userinput== user && mdpinput == mdp){
+    window.location.href = "file:///C:/Users/stagiaire/Documents/GitHub/exo_musique/assets/pages/accueil.html";
+    alert("oui")
+  }
+  else{
+    alert('merde')
+  }
+
+}
